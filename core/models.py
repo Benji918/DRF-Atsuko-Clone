e17d131a5eabb642aae4d1f322ce233b42310fbe
Benji918 <kodiugos@gmail.com>
2024-03-19 04:24:28 -0400

    Added user manager code and model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from .manager import CustomUserManager
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+import uuid
+from django.conf import settings
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    username = models.CharField(_('username'), unique=True, max_length=50, blank=False, null=False)
+    email = models.EmailField(_('email address'), unique=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
+
+    def __str__(self):
+        return self.email
