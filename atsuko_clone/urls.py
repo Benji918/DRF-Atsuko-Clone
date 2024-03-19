1affb886801e7ce2d2679acbd23198bf7ef4897e
Benji918 <kodiugos@gmail.com>
2024-03-19 10:24:52 -0400

    added djoser and swagger endpoints

--- atsuko_clone/urls.py
+++ atsuko_clone/urls.py
@@ -15,8 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('api/schema/swagger/', SpectacularSwaggerView.as_view(), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # API ENDPOINTS
+    path('api/user/', include('user.urls')),
+     path('api/schema/swagger/', SpectacularSwaggerView.as_view(), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # API ENDPOINTS
+    path('api/user/', include('user.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+
 ]
+
+if settings.DEBUG:
+    # Only include this during development to serve static files from the development server
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
