aa732ae22d1e8823262d2e1fb0d3b8905f4d7c75
Benji918 <kodiugos@gmail.com>
2024-03-19 10:26:07 -0400

    file changes

--- atsuko_clone/urls.py
+++ atsuko_clone/urls.py
@@ -21,10 +21,13 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
     SpectacularRedocView,
 )
+from django.conf.urls.static import static
+from atsuko_clone import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('api/schema/swagger/', SpectacularSwaggerView.as_view(), name='swagger-ui'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     # API ENDPOINTS
