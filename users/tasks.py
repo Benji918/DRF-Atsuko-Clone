13f331f711a70b853fe6b66276f9bfbf27521444
Benji918 <kodiugos@gmail.com>
2024-03-21 23:12:35 -0400

    integrated celery to send user registration email

--- users/tasks.py
+++ users/tasks.py
@@ -1,37 +1,35 @@
-import ssl
-from celery import shared_task
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
 import smtplib
+import smtplib, ssl
+from celery import shared_task
 from django.conf import settings
 
-@shared_task(bind=True)
-def send_registration_email(
-        sender_email=settings.SENDER_EMAIL,
-        password=settings.SENDER_EMAIL_PASSWORD,
-        receiver_email=None,
-        message='Thank you for registering on our site.'
-):
+@shared_task
+def send_registration_email( receiver_email, message='Thank you for registering on our site.'):
+    # subject = 'welcome to GFG world'
+    # message = message
+    # email_from = settings.EMAIL_HOST_USER
+    # recipient_list = [receiver_email, ]
+    # send_mail( subject, message, email_from, recipient_list )  
+
     
-    # Create a MIME object
+        # Create a MIME object
     msg = MIMEMultipart()
-    
+        
     # Attach the message
     msg.attach(MIMEText(message, "plain"))
-    
-    # Set the email subject, sender, and receiver
-    msg["Subject"] = 'Welcome to Our Site!'
-    msg["From"] = sender_email
-    msg["To"] = receiver_email
 
+    # Set the email subject, sender, and receiver
+    msg["Subject"] = 'welcome to GFG world'
+    msg["From"] = settings.EMAIL_HOST_USER
+    msg["To"] = receiver_email
 
     # Establish a connection to the SMTP server
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         # Log in to the email account
-        server.login(sender_email, password)
+        server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
         
         # Send the email
-        server.sendmail(sender_email, receiver_email, msg.as_string())
+        server.sendmail(settings.EMAIL_HOST_USER, receiver_email, msg.as_string())
