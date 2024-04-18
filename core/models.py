2fc7fa83321ee216bf50e799099599fce9580ec8
Benjamin <kodiugos@gmail.com>
2024-04-18 02:34:39 -0500

    file change

--- core/models.py
+++ core/models.py
@@ -20,62 +20,7 @@ def product_image_file(instance, filename):
     filename = f'{uuid.uuid4()}{ext}'
     return os.path.join('uploads', 'product', filename)
 
-
-# Create your models here.
-class CustomUser(AbstractUser):
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False,
-        unique=True
-    )
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
-    username = models.CharField(_('username'), unique=True, max_length=50, blank=False, null=False)
-    email = models.EmailField(_('email address'), unique=True)
-    REGISTERATION_CHOICE = [
-        ('email', 'Email'),
-        ('google', 'Google')
-    ]
-    registration_method = models.CharField(
-        max_length=10,
-        choices=REGISTERATION_CHOICE,
-        default='email'
-    )
-    is_admin = models.BooleanField(default=False)
-    is_merchant = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('username',)
-
-    def __str__(self):
-        return self.username
-
-
-class MerchantProfile(models.Model):
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False,
-        unique=True
-    )
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    profile_image = models.ImageField(upload_to=profile_image_file, null=True, blank=True)
-    merchant_name = models.CharField(max_length=100, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'Profile for {self.user.username}'
-
-
-class Product(models.Model):
-    PRODUCT_CATEGORIES = [
+PRODUCT_CATEGORIES = [
         ('electronics', 'Electronics'),
         ('clothing_apparel', 'Clothing & Apparel'),
         ('home_kitchen', 'Home & Kitchen'),
@@ -177,6 +122,60 @@ class Product(models.Model):
         ('indoor_plants_planters', 'Indoor Plants & Planters'),
         ('fireplaces_accessories', 'Fireplaces & Accessories')
     ]
+# Create your models here.
+class CustomUser(AbstractUser):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    username = models.CharField(_('username'), unique=True, max_length=50, blank=False, null=False)
+    email = models.EmailField(_('email address'), unique=True)
+    REGISTERATION_CHOICE = [
+        ('email', 'Email'),
+        ('google', 'Google')
+    ]
+    registration_method = models.CharField(
+        max_length=10,
+        choices=REGISTERATION_CHOICE,
+        default='email'
+    )
+    is_admin = models.BooleanField(default=False)
+    is_merchant = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
+
+    def __str__(self):
+        return self.username
+
+
+class MerchantProfile(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to=profile_image_file, null=True, blank=True)
+    merchant_name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Profile for {self.user.username}'
+
+
+class Product(models.Model):
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
